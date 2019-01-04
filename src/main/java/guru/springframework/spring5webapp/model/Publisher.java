4e162800dbd0a6d982f8b2f34a60882302c8367e
Bartek Szymański <github.com@draakhan.info>
2019-01-04 13:23:23 +0100

    Publisher entity was missing @Entity annotation

--- src/main/java/guru/springframework/spring5webapp/model/Publisher.java
+++ src/main/java/guru/springframework/spring5webapp/model/Publisher.java
@@ -3,10 +3,12 @@ package guru.springframework.spring5webapp.model;
 import java.util.Objects;
 import java.util.StringJoiner;
 
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+@Entity
 public class Publisher {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
